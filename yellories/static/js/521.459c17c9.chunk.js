912ed6fc3432762e824df2c97fdbe2e5276c5200
Andrea Talon <andreatalon@gmail.com>
2024-04-06 15:14:06 +0200

    404

--- yellories/static/js/521.459c17c9.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkYellories=self.webpackChunkYellories||[]).push([[521],{521:function(e,l,n){n.r(l);var t=n(540);l.default=function(){return t.createElement("div",{className:"_404"},t.createElement("h2",null,"404"),t.createElement("p",null,"Page Not Found"))}}}]);
